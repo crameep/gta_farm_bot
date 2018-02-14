b236bc8cfd009d307caa95419512a8ca017b152c
Cory Ashby <crameep@gmail.com>
2018-02-14 14:46:39 -0600

    Back to featured adversary mode - better money in it. valentines double RP events suck.

--- GTA_Farm_Bot/Scenes/FeaturedQuickjobList.cs
+++ GTA_Farm_Bot/Scenes/FeaturedQuickjobList.cs
@@ -62,6 +62,10 @@ namespace GTA_Farm_Bot.Scenes
             script.Press(new DualShockState() { DPad_Down = true });
             script.Sleep(250);
             script.Press(new DualShockState() { DPad_Down = true });
+            script.Sleep(250);
+            script.Press(new DualShockState() { DPad_Down = true });
+            script.Sleep(250);
+            script.Press(new DualShockState() { DPad_Down = true });
 
         }
     }
